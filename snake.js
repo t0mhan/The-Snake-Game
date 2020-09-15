1e2e5850f1a693a7ef85eda10678c4e5cccfb061
singh@tourismussuite.com <resopankaj@gmail.com>
2020-09-15 13:49:10 +0200

    html update

--- snake.js
+++ snake.js
@@ -1,19 +1,18 @@
-
-function Snake(){
-    this.show = function(){
+function Snake() {
+    this.show = function() {
         fill(255);
         //draw the snake tail
-        for(var i=0;i<this.tail.length;i++){
+        for (var i = 0; i < this.tail.length; i++) {
             rect(this.tail[i].x, this.tail[i].y, pixel_size, pixel_size);
         }
         //draw the snake head
         rect(this.pos.x, this.pos.y, pixel_size, pixel_size)
     }
-    
-    this.update = function(){
+
+    this.update = function() {
         //move snake's position into tail and pop off the end
-        if(movement.length){
-            if(snake.speed.x != movement[0][0]*-1 && snake.speed.y != movement[0][1]*-1){
+        if (movement.length) {
+            if (snake.speed.x != movement[0][0] * -1 && snake.speed.y != movement[0][1] * -1) {
                 snake.dir(movement[0][0], movement[0][1]);
             }
             movement.splice(0, 1);
@@ -26,27 +25,27 @@ function Snake(){
         this.pos.y += this.speed.y * pixel_size;
     }
 
-    this.dir = function(x, y){
+    this.dir = function(x, y) {
         this.speed.x = x;
         this.speed.y = y;
     }
 
-    this.checkDeath = function(){
-        if(this.pos.x >= width || this.pos.y >= height || this.pos.x < 0 || this.pos.y < 0){
+    this.checkDeath = function() {
+        if (this.pos.x >= width || this.pos.y >= height || this.pos.x < 0 || this.pos.y < 0) {
             gameState = 'end';
         }
-        for(var i=0;i<this.tail.length;i++){
-            if(this.tail[i].x == this.pos.x && this.tail[i].y == this.pos.y){
+        for (var i = 0; i < this.tail.length; i++) {
+            if (this.tail[i].x == this.pos.x && this.tail[i].y == this.pos.y) {
                 gameState = 'end';
             }
         }
     }
 
-    this.eat = function(pos){
+    this.eat = function(pos) {
         return this.pos.x == pos.x && this.pos.y == pos.y;
     }
 
-    this.reset = function(){
+    this.reset = function() {
         shots = [];
         this.tail = [];
         this.pos = createVector(0, 0);
